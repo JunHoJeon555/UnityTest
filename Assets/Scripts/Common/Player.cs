98f447be95193d1b0fc51ca60d8974f648333535
junho_Jeon <115610258+Pigon5@users.noreply.github.com>
2023-03-07 14:30:13 +0900

    0307 - PowerUp 성능을 만듬

--- Assets/Scripts/Common/Player.cs
+++ Assets/Scripts/Common/Player.cs
@@ -11,20 +11,49 @@ public class Player : MonoBehaviour
     public float speed = 10.0f;                //player 속도
     public PoolObjectType bulletType;                  //퍼블릭으로 게임오브젝트라는 변수 선언, Bullet이라는 변수 생성
 
-                       // Transform클래스 선언 fireTransform 변수 생성
+                         // Transform클래스 선언 fireTransform 변수 생성
     private GameObject fireFlash;              // GameObject클래스 선언 fireFlash를 생성
 
     private Rigidbody2D rigid;
-    Animator anim;                             //
-    Transform fireTransform;
+    //private Transform fireRoot;
 
+    private Transform [] fireTransforms;
+
+    Animator anim;                             
 
     //입력철용 InputAction
     PlayerInputActions inputActions;           //PlayerInputActions 클래스 선언 inputActions 변수할당 
     Vector3 inputDir = Vector3.zero;           //Vector3클래스에 inputDir 은 0,0,0 좌표를 얻었다.
 
     /// 연사용 코루틴을 저장할 변수
-    IEnumerator fireCoroutine;                 
+    IEnumerator fireCoroutine;
+
+   
+    //현재 파워
+    public int power = 0;
+
+    float fireAngle = 30.0f;
+    //파워가 최대치일 때 파워업 아잍메 먹으며ㅓㄴ 보너스
+    int extraPowerBonus = 300;
+
+    //파워를 증감시키기 위한 프로퍼티 (설정시 추가처리 잇음)
+    int Power
+    {
+        get => power;
+        set
+        {
+            power= value;
+            if (power > 3) //3을 넘어가면 
+            {
+                AddScore(extraPowerBonus); //보너스 추가
+                power = Mathf.Clamp(power, 1, 3); //1~3사이로 설정되게 Clmp처리
+            }
+            RefreshFirePostions(power);         //FireTransform의 위치와 회전 처리 
+        }
+
+    }
+
+   
 
     //플레이어 점수
     int score = 0;
@@ -32,6 +61,7 @@ public class Player : MonoBehaviour
     //델리게이트 //delegate : 신호를 보내는 것, 함수를 등록할 수 있다
     //점수가 변경되면 실행될 델리게이트 . 파라메터가 int 하나이고 리턴타입이 void인 함수를 등록할 수 있다.
     public Action<int> onScoreChange;
+
 
     //property : 값을 넣거나 읽을 때 추가적으로 할일이 많을 때 사용한다.
     //플레이어의 점수를 확인할 수 있다.
@@ -70,8 +100,15 @@ public class Player : MonoBehaviour
                                                 //Update함수에 절대 넣으면 안된다.
         rigid = GetComponent<Rigidbody2D>();
         inputActions = new PlayerInputActions();
-        fireTransform = transform.GetChild(0);  //fireTransform이라는 변수가 자식넘버가 0이다. 즉, 1번째 자식.
+        Transform fireRoot = transform.GetChild(0);  //fireTransform이라는 변수가 자식넘버가 0이다. 즉, 1번째 자식.
+        fireTransforms = new Transform[fireRoot.childCount];
+        for(int i = 0; i < fireRoot.childCount; i++)
+        {
+            fireTransforms[i] = fireRoot.GetChild(i);
+        }
+
         fireFlash = transform.GetChild(1).gameObject;
+
         fireFlash.SetActive(false);
 
 
@@ -118,6 +155,7 @@ public class Player : MonoBehaviour
     //업데이트가 실행하기 전. 시작할 때 실행되는 함수 
     void Start()
     {
+        power = 1; //power는 1로 시작 
        // Debug.Log("Start");
     }
      
@@ -163,6 +201,11 @@ public class Player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log($"충돌영역에서 들어감 : 충돌 대상 : {collision.gameObject.name}");
+        if (collision.gameObject.CompareTag("PowerUp"))
+        {
+            Power++;
+            collision.gameObject.SetActive(false);
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
@@ -203,9 +246,15 @@ public class Player : MonoBehaviour
     {
         while(true)
         {
+            //obj.transform.position = fireTransform.position;
+            for(int i = 0; i<power; i++)
+            {
 
-            GameObject obj = Factory.Inst.GetObject(bulletType);                 //총알생성
-            obj.transform.position = fireTransform.position;
+               GameObject obj = Factory.Inst.GetObject(bulletType);                 //총알생성
+               Transform firePos = fireTransforms[i];
+               obj.transform.position = firePos.position;
+               obj.transform.rotation = firePos.rotation;
+            }
             StartCoroutine(FlashEffect());
             yield return new WaitForSeconds(fireInterval);       //연사간격만큼 대기
         }
@@ -238,7 +287,38 @@ public class Player : MonoBehaviour
     {
         Score += plus;
     }
-    
+
+    private void RefreshFirePostions(int power)
+    {
+        //기존fireRoot에 자식 비활성화 하기
+
+        //fireRoot.childCount;
+        for(int i =0; i< fireTransforms.Length; i++)
+        {
+            fireTransforms[i].gameObject.SetActive(false);
+        }
+
+       //fireRoot에 power 숫자에 맞게 자식 활성화
+       for(int i=0; i<power; i++)
+        {
+            //파워1 :  0 오른쪽 벡터 0도만큼 회전
+            //파워2 : -15 15 1)오른쪽 벡터를 fireAngle * -0.5 2) 오른쪽 벡터를 fireAngle *0.5
+            //파워3 : -30 0 30 1)오른쪽 벡터 fireAngle*-1 2)오른쪽 벡터 fireAngle*0 3)오른쪽 벡터 fireAngle*1 
+
+            Transform firePos = fireTransforms[i];
+            firePos.localPosition = Vector3.zero;
+            firePos.rotation = Quaternion.Euler(0,0,(power-1)*(fireAngle*0.5f) + i * -fireAngle);
+            firePos.Translate(1, 0, 0);
+
+            //연산식에 대입해보면 
+            //파워가 1일 때 0도 i가 1번반복
+            //파워가 2일 때 15, -15 2번반복
+            //파워가 3일 때 30 0 30 3번반복 
+
+            fireTransforms[i].gameObject.SetActive(true) ;
+        }
+    }
+
 
 }
 
--- Assets/Scripts/Common/SpawnObjects/PowerUp.cs
+++ Assets/Scripts/Common/SpawnObjects/PowerUp.cs
@@ -5,23 +5,52 @@ using UnityEngine;
 public class PowerUp : PoolObject
 {
     //이동 속도
-    public float moverSpeed = 0.5f;
+    public float moverSpeed = 2.0f;
     //방향전환하는 시간간격
-    public float dirChangeInterval = 1.0f;
+    public float dirChangeInterval = 5.0f;
 
+    //마지막 단계 강제진행
+    public float timeOut = 15.0f;
+    
     //플레리어의 트랜스폼
     Transform playerTransform;
     //이동방향
     Vector2 dir;
     //코루틴으로 미리 계산해 놓은 변수
     WaitForSeconds changeInterval;
+    
 
 
+    // 최대 튕기는 횟수를 기록한 상수
+    const int DirChangeCountMax = 5;
+
+    // 현재 남아있는 튕길 횟수를 저장하는 변수
+    int dirChangeCount = DirChangeCountMax;
+
+    
+
+    //g현재 탐아있는 튕길 횟수를 조정하고  설정하는 프로퍼티
+    int DirChangeCount
+    {
+        get => dirChangeCount; //읽는거 맘대로
+        set 
+        {
+            dirChangeCount = value; //쓸떄는 0이야가 되면 특정행동을 처리
+            anim.SetInteger("Count" ,dirChangeCount);
+            if(dirChangeCount <=0)
+            {
+                StopAllCoroutines(); //모든 코루틴 정지
+            }
+        }
+
+    }
+    Animator anim;
     private void Awake()
     {
         changeInterval = new WaitForSeconds(dirChangeInterval);
+        anim = GetComponent<Animator>();
     }
-
+    
     private void OnEnable()
 
     {
@@ -35,8 +64,12 @@ public class PowerUp : PoolObject
         }
         SetRandomDirection(true); //시작할 때 랜덤 방향 설정하기
 
+
+        DirChangeCount = DirChangeCountMax;// 튕기는 횟수 초기화
         StopAllCoroutines();
+        StartCoroutine(TimeOut());
         StartCoroutine(DirChange());
+       
 
 
 
@@ -44,12 +77,12 @@ public class PowerUp : PoolObject
 
     private void Update()
     {
-        transform.Translate(Time.deltaTime * dir); //방향대로 이동시키기
+        transform.Translate(Time.deltaTime *moverSpeed *dir); //방향대로 이동시키기
     }
     private void SetRandomDirection(bool allRandom = false)
     {
-        dir = Random.insideUnitCircle; //반지름이 1인 원 안의 랜덤한 위치 가져오기
-        if(!allRandom && Random.value < 0.3f)
+        //dir = Random.insideUnitCircle; //반지름이 1인 원 안의 랜덤한 위치 가져오기
+        if(!allRandom && Random.value < 0.4f)
         {
             // 완전 랜덤이 아니고 40%의 확률에 당첨이 되면 플레이어의 반대 방향으로 이동시키기
             Vector2 playerToPowerUp =transform.position - playerTransform.position;  // 플레이어에게 가는 방향벡터 
@@ -66,7 +99,7 @@ public class PowerUp : PoolObject
         }
 
             dir = dir.normalized;          // 길이를 1로 만들어서 항상동일한 속도가 되게 만들기
-
+            dirChangeCount--; //충돌할 때도 dircount하겠다./ dir 감소
     }
 
     IEnumerator DirChange()
@@ -78,11 +111,23 @@ public class PowerUp : PoolObject
         }
     }
 
+    IEnumerator TimeOut()
+    {
+        yield return new WaitForSeconds(timeOut);
+        DirChangeCount = 0;
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Border"))
+        if (DirChangeCount >0 &&    gameObject.CompareTag("Border"))
         {
-            dir = Vector2.Reflect(dir, collision.contacts[0].normal); //보더에 부딪치면 방향전환하기
+            // 튕길 횟수가 남아있고 Border와 부딪쳤으면
+            dir = Vector2.Reflect(dir, collision.contacts[0].normal); //보더에 부딪치면 방향전환하기 방향전환하기
+            DirChangeCount--;                                       // dirChangeCount 감소
+
+            //anim.GetCurrentAnimatorClipInfoCount(+1);
         }
+        
     }
 }
